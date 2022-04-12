5cf1e7aa6ee477b6b3401172d75650b3bb754f53
giancarlorhodes <giancarlo.rhodes@gmail.com>
2022-04-12 14:50:36 -0500

    bug in tests

--- UnitTestSorting/UnitTestSort.cs
+++ UnitTestSorting/UnitTestSort.cs
@@ -67,69 +67,183 @@ namespace UnitTestSorting
 
             // act
             _listUnsorted = _sort.GenerateIntList(100);
-            List<int> _listSorted = _sort.Sorting(_listUnsorted);
+            List<int> _listSorted = _sort.Sorting(_listUnsorted); // desc
             // List<int> _testing = new List<int> { 1, 2, 6, 7 };
 
             // assert
             Assert.AreEqual(_listSorted.Count, _listUnsorted.Count); // same number of elements
 
             // pass in a unsorted list of integers and get back descending list  1, 123, 2345 ....
-            bool IsUnorderedDescending = true;
+            bool IsUnordered = true;
             for (int i = 0; i < _listSorted.Count; i++)
             {
-                if (i > 0)
+                if (i > 0) // make sure we are in index range
                 {
-                    if (_listSorted[i] > _listSorted[i - 1])
+                    if (_listSorted[i] > _listSorted[i - 1] || _listSorted[i] == _listSorted[i - 1])
                     {
                         continue;
                     }
                     else 
                     {
-                        IsUnorderedDescending = false;
+                        IsUnordered = false;
                         break;
                     }
                 }                
             }
 
-            Assert.IsTrue(IsUnorderedDescending);
+            Assert.IsTrue(IsUnordered);
         }
 
 
         [TestMethod]
-        public void Sort_1000_integers_ascending()
+        public void Sort_100_integers_between_1_and_100000_ascending()
         {
 
             // arrange
             List<int> _listUnsorted = new List<int>();
+            //_listUnsorted = new List<int> { 100, 21, 3 }; // ascending
 
             // act
-            _listUnsorted = _sort.GenerateIntList(1000, 1, 100000);
+            _listUnsorted = _sort.GenerateIntList(100, 1, 100000);
+
             List<int> _listSorted = _sort.Sorting(_listUnsorted, AscendingOrDescending.Ascending);
 
 
             // assert
             Assert.AreEqual(_listSorted.Count, _listUnsorted.Count); // same number of elements
 
-            // pass in a unsorted list of integers and get back descending list  1, 123, 2345 ....
-            bool IsUnorderedDescending = true;
+            // pass in a unsorted list of integers and get back ascending list 
+            bool IsUnordered = true;
             for (int i = 0; i < _listSorted.Count; i++)
             {
                 if (i > 0)
                 {
-                    if (_listSorted[i] < _listSorted[i - 1])
+                    if (_listSorted[i] < _listSorted[i - 1] || _listSorted[i] == _listSorted[i - 1])
                     {
                         continue;
                     }
                     else
                     {
-                        IsUnorderedDescending = false;
+                        IsUnordered = false;
                         break;
                     }
                 }
             }
 
-            Assert.IsTrue(IsUnorderedDescending);
+            Assert.IsTrue(IsUnordered);
         }
+
+
+        [TestMethod]
+        public void Sort_1000_integers_between_1_and_100_ascending()
+        {
+
+            // arrange
+            List<int> _listUnsorted = new List<int>();
+            //_listUnsorted = new List<int> { 100, 100, 21, 3, 3, 3  }; // ascending
+
+            // act
+            _listUnsorted = _sort.GenerateIntList(1000, 1, 100);
+
+            List<int> _listSorted = _sort.Sorting(_listUnsorted, AscendingOrDescending.Ascending);
+
+
+            // assert
+            Assert.AreEqual(_listSorted.Count, _listUnsorted.Count); // same number of elements
+
+            // pass in a unsorted list of integers and get back ascending list 
+            bool IsUnordered = true;
+            for (int i = 0; i < _listSorted.Count; i++)
+            {
+                if (i > 0)
+                {
+                    if (_listSorted[i] < _listSorted[i - 1] || _listSorted[i] == _listSorted[i - 1])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        IsUnordered = false;
+                        break;
+                    }
+                }
+            }
+
+            Assert.IsTrue(IsUnordered);
+        }
+
+        [TestMethod]
+        public void Sort_1000_integers_between_neg100_and_pos100_descending()
+        {
+
+            // arrange
+            List<int> _listUnsorted = new List<int>();
+            //_listUnsorted = new List<int> { -100, -100, -21, -3, 3, 3 }; // descending
+
+            // act
+            _listUnsorted = _sort.GenerateIntList(1000, -100, 100);
+
+            List<int> _listSorted = _sort.Sorting(_listUnsorted, AscendingOrDescending.Descending);
+
+            // assert
+            Assert.AreEqual(_listSorted.Count, _listUnsorted.Count); // same number of elements
+
+            // pass in a unsorted list of integers and get back ascending list 
+            bool IsUnordered = true;
+            for (int i = 0; i < _listSorted.Count; i++)
+            {
+                if (i > 0)
+                {
+                    if (_listSorted[i] > _listSorted[i - 1] || _listSorted[i] == _listSorted[i - 1])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        IsUnordered = false;
+                        break;
+                    }
+                }
+            }
+            Assert.IsTrue(IsUnordered);
+        }
+
+
+        [TestMethod]
+        public void Sort_1000_integers_between_neg1000_and_pos1000_descending()
+        {
+
+            // arrange
+            List<int> _listUnsorted = new List<int>();
+           
+            // act
+            _listUnsorted = _sort.GenerateIntList(1000, -1000, 1000);
+
+            List<int> _listSorted = _sort.Sorting(_listUnsorted, AscendingOrDescending.Descending);
+
+            // assert
+            Assert.AreEqual(_listSorted.Count, _listUnsorted.Count); // same number of elements
+
+            // pass in a unsorted list of integers and get back ascending list 
+            bool IsUnordered = true;
+            for (int i = 0; i < _listSorted.Count; i++)
+            {
+                if (i > 0)
+                {
+                    if (_listSorted[i] > _listSorted[i - 1] || _listSorted[i] == _listSorted[i - 1])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        IsUnordered = false;
+                        break;
+                    }
+                }
+            }
+            Assert.IsTrue(IsUnordered);
+        }
+
 
     }
 }
